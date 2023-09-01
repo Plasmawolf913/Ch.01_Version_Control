b23df5fec44b07919cc489d81ffc2ce32a416f1c
folkerstru <trumanjackm@gmail.com>
2023-09-01 08:59:26 -0500

    My 1st commit

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -3,19 +3,19 @@
 
 
 1. Define Forking (1pt): 
-
+Copying a code template from a repository on to your local repository.
 2. Define Cloning (1pt): 
-
+Downloading the code from your repository to your local device.
 3. Define Branching (1pt):
-
+Using the master code to make a separate "branch" of your code that can be uploaded to the master code.
 4. Define Committing (1pt): 
-
+Saving your code.
 5. Define Merging (1pt): 
-
+Putting branches or code into the master code.
 6. Define Pushing (1pt):
-
+Uploading code from your device to your repository.
 7. Define Pull Request (1pt):
-
+Requesting to have your code uploaded to the master code in the main repository.
 
 8. TURTORIAL ART (10pts.)
 
@@ -25,29 +25,46 @@ Your first and last name must be written on your art.
 The last line keeps the window open until you click to close.
 Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 '''
+
 import turtle
-yoda=turtle.Turtle()
-screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
+import random
+
+tina = turtle.Turtle()
+tommy = turtle.Turtle()
+
+tina.color('blue')
+tommy.color('green')
+
+tina.shape('turtle')
+tommy.shape('turtle')
+tommy.speed(1000)
+tina.speed(1000)
+
+tina.ht()
+tommy.pendown()
+turtle.bgcolor('black')
+tommy.ht()
+
+color_list = ['red','orange','yellow']
+
+for number in range(150):
+    tommy.forward(number+1)
+    tina.forward(number+1)
+    tommy.left(75)
+    tina.left(55)
+    if number%2==0:
+        tommy.color(color_list[0])
+        tina.color(color_list[0])
+    else:
+        tommy.color(color_list[1])
+        tina.color(color_list[1])
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+tina.penup()
+tina.goto(175,-160)
+tina.color('red')
+tina.write('"Rose"',font=("Arial", 16, "normal"))
+tina.goto(175,-175)
+tina.color('red')
+tina.write('Truman Folkers',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
--- README.md
+++
@@ -1,7 +0,0 @@
-<img align="left" src="http://hermonswebsites.com/Classes/CS/python.png"><H2>Chapter 1 Version Control Systems and Github</H2>
-
-
-
-In this chapter you will be practicing forking, cloning, branching, committing, merging, pushing and pull requesting. You will also create and present a piece of Turtle Art.
-
-  </ol>
